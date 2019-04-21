62846052413c03228024d44175c0c7ea5a67fc54
Alexsandr <Alex45v90@mail.ru>
2019-04-21 21:44:01 +0300

    Edit class Data

--- src/Data.java
+++ src/Data.java
@@ -1,24 +1,44 @@
-import java.util.Date;
+import java.util.*;
 
 public class Data {
 
-    private Film[] films = new Film[] {
-            new Film("1","фильм","Джанго","драма",new Date(),"Эксцентричный охотник за головами, также известный как «Дантист» промышляет отстрелом самых опасных преступников", 7.1),
-            new Film("2","фильм","Донни Браско","драма",new Date(),"Нью-Йорк, 1978 год. Агент ФБР Джо Пистоне получает новое задание: внедриться в одну из преступных группировок Бруклина.", 7.9),
-            new Film("3","фильм","Жестокий романс","мелодрама",new Date(),"Действие разворачивается на берегу Волги в вымышленном провинциальном городке Бряхимове в 1877-1878 годах.", 8.1),
-    };
+    private Map<String,String> users;
+    private ArrayList<Film> films;
 
-    private User[] users = new User[] {
-            new User("Alex45","Alex","12345"),
-            new User("Anton10","Anton","1234"),
-    };
+    public Data() {
+        this.users = this.addUsers();
+        this.films = this.addFilms();
+    }
 
-    public  Film[] getFilms() {
+    private Map<String,String> addUsers() {
+        Map<String,String> usersMap = new HashMap<String,String>();
+
+        usersMap.put("Alex","12345");
+        usersMap.put("Anton","1234");
+
+        return usersMap;
+    }
+
+
+    private ArrayList<Film> addFilms() {
+
+        ArrayList<Film>  filmsArray = new ArrayList<Film>(){
+            {
+                add(new Film("1","фильм","Джанго","драма","2008","Эксцентричный охотник за головами, также известный как «Дантист» промышляет отстрелом самых опасных преступников", 7.1, "tt0119008"));
+                add(new Film("2","фильм","Донни Браско","драма","1998","Нью-Йорк, 1978 год. Агент ФБР Джо Пистоне получает новое задание: внедриться в одну из преступных группировок Бруклина.", 7.9,"tt0149008"));
+                add(new Film("3","фильм","Жестокий романс","мелодрама","1987","Действие разворачивается на берегу Волги в вымышленном провинциальном городке Бряхимове в 1877-1878 годах.", 8.1,"tt0112005"));
+            }
+        };
+
+        return filmsArray;
+    }
+
+    public  ArrayList<Film> getFilms() {
         return films;
     }
 
-    public  User[] getUser() {
-        return users;
+    public Map<String,String> getUsers() {
+        return this.users;
     }
 
 }
