1b9c52b6611d5088f096c1e2d063c394a2cb96d5
Alexsandr <Alex45v90@mail.ru>
2019-04-21 21:43:47 +0300

    Edit class Film

--- src/Film.java
+++ src/Film.java
@@ -1,15 +1,19 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Film {
+
     private String id;
     private String type;
     private String name;
     private String genre;
-    private Date date;
+    private String date;
     private String desk;
-    private double rating;
+    private Double rating;
+    private String imdb;
+    private ArrayList<Comment> comment = new ArrayList<>();
 
-    public Film(String id, String type, String name, String genre, Date date, String desk, double rating) {
+    public Film (String id, String type, String name, String genre, String date, String desk, Double rating, String imdb) {
         this.id = id;
         this.type = type;
         this.name = name;
@@ -17,9 +21,46 @@ public class Film {
         this.date = date;
         this.desk = desk;
         this.rating = rating;
+        this.imdb = imdb;
     }
 
-    public String getName(){
+    public void addComment(String autor, String text, double rating) {
+        comment.add(new Comment(autor, text, rating));
+    }
+
+    public ArrayList<Comment> getComment() {
+        return comment;
+    }
+
+    public String getImdb() {
+        return imdb;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
         return name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDesk() {
+        return desk;
+    }
+
+    public double getRating() {
+        return rating;
     }
 }
